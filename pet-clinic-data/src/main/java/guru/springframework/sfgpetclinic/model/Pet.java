547b88c901cbe4a1f1b6ed76e0ea265bfdd961cc
jasio93z@gmail.com <Lodyyy13>
2019-11-05 21:17:19 +0100

    update project

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,34 +1,36 @@
 package guru.springframework.sfgpetclinic.model;
 
-import java.time.LocalDate;
+import lombok.*;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
-    private LocalDate brithDate;
 
-    public PetType getPetType() {
-        return petType;
-    }
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
 
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBrithDate() {
-        return brithDate;
-    }
-
-    public void setBrithDate(LocalDate brithDate) {
-        this.brithDate = brithDate;
-    }
 }
